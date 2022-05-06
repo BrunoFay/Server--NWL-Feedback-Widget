7dc4cbacb984cdde63f236bebe281ac82669e0d6
BRUNO <brunofay1@hotmail.com>
2022-05-06 18:26:55 -0300

    FIX error Middleware

--- src/middlewares/handleErrors.ts
+++ src/middlewares/handleErrors.ts
@@ -1,6 +1,4 @@
 import { ErrorRequestHandler } from 'express'
 export const handleErrors: ErrorRequestHandler = (err, req, res, next) => {
-  if (err) {
-    return res.status(500).json({ message: `${err}` })
-  }
+  return res.status(500).json({ message: `${err}` })
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { nextTick } from "process";
 import { FeedbackModel } from "../model";
 import { FeedbackService } from "../service";
 
@@ -6,12 +7,16 @@ const feedbackRoute = Router()
 feedbackRoute.get('/', (req, res) => {
   return res.status(200).json({ message: 'server online' })
 })
-feedbackRoute.post('/feedbacks', async (req, res) => {
-  const model = new FeedbackModel()
-  const service = new FeedbackService(model)
-  const newFeedback = await service.createFeedback(req.body)
-  await service.sendEmail(req.body)
+feedbackRoute.post('/feedbacks', async (req, res, next) => {
+  try {
+    const model = new FeedbackModel()
+    const service = new FeedbackService(model)
+    const newFeedback = await service.createFeedback(req.body)
+    await service.sendEmail(req.body)
+    return res.status(201).json(newFeedback)
 
-  return res.status(201).json(newFeedback)
+  } catch (error) {
+    next(error)
+  }
 })
 export default feedbackRoute
