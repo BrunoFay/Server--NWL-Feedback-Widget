b9791fec4db1fdd24d72afb8ec0d7602372d3790
BRUNO <brunofay1@hotmail.com>
2022-05-06 16:58:34 -0300

    REFACT initial route test to deploy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import { json } from "stream/consumers";
 import { FeedbackModel } from "../model";
 import { FeedbackService } from "../service";
 
 const feedbackRoute = Router()
+feedbackRoute.get('/', (req, res) => {
+  return res.status(200).json({ message: 'server online' })
+})
 feedbackRoute.post('/feedbacks', async (req, res) => {
   const model = new FeedbackModel()
   const service = new FeedbackService(model)
