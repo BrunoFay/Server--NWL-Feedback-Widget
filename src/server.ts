8f3a5b796541607b6e9c0cdbd2b4449510b8cf12
BRUNO <brunofay1@hotmail.com>
2022-05-06 20:30:29 -0300

    test

--- src/server.ts
+++ src/server.ts
@@ -6,18 +6,8 @@ import { handleErrors } from './middlewares/handleErrors'
 const app = express()
 const PORT = process.env.PORT || 3333
 
-/* app.use(cors({
-  origin: process.env.FRONT_URL,
-  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"]
-})) */
-app.use(cors(), (req, res, next)=> {
-  res.header("Access-Control-Allow-Origin", process.env.FRONT_URL)
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+ app.use(cors()) 
+
 app.use(express.json())
 app.use(feedbackRoute)
 
