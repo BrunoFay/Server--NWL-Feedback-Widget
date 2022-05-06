cb47f04b178bebcb75d5413c1a9086727e17ca0b
BRUNO <brunofay1@hotmail.com>
2022-05-06 19:40:28 -0300

    fix url

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,11 @@ import { handleErrors } from './middlewares/handleErrors'
 
 const app = express()
 const PORT = process.env.PORT || 3333
-
-app.use(cors({ origin: process.env.FRONT_URL ,methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],}))
+/*  */
+app.use(cors({
+  origin: process.env.FRONT_URL,
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"]
+}))
 app.use(express.json())
 app.use(feedbackRoute)
 
