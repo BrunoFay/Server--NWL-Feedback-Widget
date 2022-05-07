2dec0c9d850cc87d74cdac7c051a2ea5467d8fd6
BRUNO <brunofay1@hotmail.com>
2022-05-06 21:36:44 -0300

    test

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,10 @@ const PORT = process.env.PORT || 3333
 app.use(cors({
   origin: 'https://nlw-feedback-widget-1l32u0v6a-brunofay.vercel.app/'
 }))
-app.use((req, res, next) => {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.use(express.json())
