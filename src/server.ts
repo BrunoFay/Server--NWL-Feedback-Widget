214654c7f98fba4de9d971056b8febd721743b02
BRUNO <brunofay1@hotmail.com>
2022-05-06 21:31:44 -0300

    test

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ app.use(cors({
   origin: 'https://nlw-feedback-widget-1l32u0v6a-brunofay.vercel.app/'
 }))
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://nlw-feedback-widget-1l32u0v6a-brunofay.vercel.app/");
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 app.use(express.json())
