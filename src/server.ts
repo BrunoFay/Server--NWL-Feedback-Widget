f52b729b6062cfd53f75d569b363e4d62cc22b07
BRUNO <brunofay1@hotmail.com>
2022-05-06 22:17:36 -0300

    FIX url cors

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3333
 
 
 app.use(cors(), function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://nlw-feedback-widget-tau.vercel.app/"); 
+  res.header("Access-Control-Allow-Origin", "https://nlw-feedback-widget-tau.vercel.app"); 
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
