3db43ce292bbbb34629eb96188fb17c97cd1eb0d
BRUNO <brunofay1@hotmail.com>
2022-05-06 22:11:18 -0300

    TEST

--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,17 @@ import { handleErrors } from './middlewares/handleErrors'
 const app = express()
 const PORT = process.env.PORT || 3333
 
-app.use(cors({
-  origin: 'https://nlw-feedback-widget-tau.vercel.app/'
-}))
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+
+app.use(cors(), function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://nlw-feedback-widget-tau.vercel.app/"); 
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
+
+
 app.use(express.json())
 app.use(feedbackRoute)
 
