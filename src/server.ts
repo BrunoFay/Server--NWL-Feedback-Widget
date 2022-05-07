d6317be91825dfa6712fab99bee1122d6fba7097
BRUNO <brunofay1@hotmail.com>
2022-05-06 22:04:16 -0300

    test

--- src/server.ts
+++ src/server.ts
@@ -11,8 +11,6 @@ app.use(cors({
 }))
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'POST,');
-
   next();
 });
 app.use(express.json())
